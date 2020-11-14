8140d4378eaedf80db4cb395470a4942de708139
tamanarasuli <tamana.rasuli@gmail.com>
2020-11-14 02:32:46 +0000

    error handling

--- static/track.js
+++ static/track.js
@@ -1,32 +1,36 @@
 console.log("Hello world");
 
-function send() {
-    //console.log("name" + name);
-    let name =$("#username").val();
-    let goal = $("#quantity").val(); 
-    let intake = $("#goal").val(); 
+function send(name, goal, intake) {
+     name =$("#username").val();
+     goal = $("#quantity").val(); 
+     intake = $("#goal").val(); 
     if (name !== ""){
       let url = "/insertData/name/" + name + "/goal/" + goal + "/intake/" + intake;
     console.log(url); 
       window.location.replace(url);
     }
-    // let url = "/insert/name/" + name + "/goal/" + goal + "/intake/" + intake;
-    // console.log(url);
-    // window.location.replace(url);
+    
 }
+function negative(goal, intake) {
+    let flag = false; 
+    if (goal < 0 || intake < 0) {
+        alert("Enter a valid cup amount, please "); 
+            return true; 
+     } else {
+         send("intake", name, goal, intake); 
+    }
+}
+
+
+function doNegative() {
+    negative(goal, intake);
+}
+
  
 
 function setup() {
     console.log("I have linked Javascript!");
-    let name = $("#username").val(); 
-    console.log(name);
-    console.log($("#username").val());
-    console.log(name); 
-     
-    //send("x",1,1);
-    //  $("#intake").click(send($("#username").val(), $("quantity").val(), $("quantity").val()));
-    
-      $("#intake").click(send());
+    $("#intake").click(doNegative);
 }
 
 $(document).ready(setup);
