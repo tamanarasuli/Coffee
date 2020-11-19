cf09f701f03835527470546b2e43d6d51985d375
tamanarasuli <tamana.rasuli@gmail.com>
2020-11-19 01:24:06 +0000

    insert + error handling

--- static/track.js
+++ static/track.js
@@ -12,6 +12,15 @@ function send(name, goal, intake) {
     
 }
 
+function insert(email, username, password) {
+      let url = "/insertSignUp/email/" + email + "/username/" + username + "/password/" + password;
+    console.log(url); 
+      window.location.replace(url);
+      console.log("made it to the insert to the signup function"); 
+    
+}
+
+
 // create  this function for all error handling 
 function negative(name, goal,  intake) {
     name =$("#username").val();
@@ -30,6 +39,27 @@ function negative(name, goal,  intake) {
     } else {
         alert("Congratulations, you have met your goal!"); 
           send(name, goal, intake); 
+    }
+}
+
+function signUp(email, name, password) {
+    console.log("in signup");
+    email = $("#email").val(); 
+    username = $("#name").val(); 
+    password = $("#pass").val(); 
+    console.log(email); 
+    
+    if(password == " " || username == " " || email == " ") {
+        alert("enter a password, please"); 
+    }
+    // if (password.length() <  8) {
+    //     alert("enter at least 8 characters, please"); 
+    // }
+    // if(email.charAt(i)!= '@') {
+    //     alert("please type in a valid email address.");
+    
+        else {
+        insert(email, username, password); 
     }
 }
 
@@ -52,6 +82,7 @@ function handleRespones() {
 function setup() {
     console.log("I have linked Javascript!");
     $("#intake").click(handleRespones);
+    $('#signup').click(signUp);
 }
 
 $(document).ready(setup);
